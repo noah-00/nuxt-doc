7e6eff651a7b7613bcb3d43f2c736dc1de1274c0
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-10-01 21:39:16 +0900

    ✨ layout in App.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+const admin = true; // auth function
+const layout = admin ? "default" : "custom";
+</script>
+
 <template>
-  <NuxtLayout>
+  <NuxtLayout :name="layout">
     <Header></Header>
     <NuxtPage></NuxtPage>
   </NuxtLayout>
--- pages/layoutImport.vue
+++ pages/layoutImport.vue
@@ -1,14 +1,14 @@
 <script setup>
 // if you want to use the default layout, you don't need to do anything.
 // But if you want to use a custom layout, you can use the definePageMeta function.
-definePageMeta({
-  layout: "custom",
-});
+// definePageMeta({
+//   layout: "custom",
+// });
 
 // if you want to disable the default layout, you can use the definePageMeta function.
-definePageMeta({
-  layout: false,
-});
+// definePageMeta({
+//   layout: false,
+// });
 </script>
 
 <template>
