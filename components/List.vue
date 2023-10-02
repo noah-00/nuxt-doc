3fa9f9c2a277bca02b37ff1ded5e325589a4a03f
noah-00 <t71.ishizaka.naoya@gmail.com>
2023-10-03 08:50:05 +0900

    ✨ error handling

---
+++ components/List.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>ユーザリスト</h1>
+    <h2>Counter</h2>
+    <p>Count: {{ counter }}</p>
+    <div><button @click="inc">+</button></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const counter = ref(0);
+const inc = () => {
+  throw createError("エラー発生");
+  counter.value++;
+};
+</script>
---
+++ pages/errorHandling/index.vue
@@ -0,0 +1,22 @@
+<script setup>
+const resetError = (error) => {
+  error.value = null;
+};
+const someErrorLogger = (error) => {
+  // here your error handling logic
+  console.log("someErrorLogger", error);
+};
+</script>
+
+<template>
+  <div>
+    <h1>Error Handling</h1>
+    <NuxtErrorBoundary @error="someErrorLogger">
+      <List />
+      <template #error="{ error }">
+        <p>{{ error }}</p>
+        <button @click="resetError(error)">Clear Error</button>
+      </template>
+    </NuxtErrorBoundary>
+  </div>
+</template>
